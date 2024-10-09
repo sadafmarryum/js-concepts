a5748fb0a68d1f24cb1f58f3fbd3ed6837426fd8
Sadaf <sadaf.marryum@gmail.com>
2024-10-09 18:07:43 +0500

    write code using conditional statement(check grade according to their scores).

--- basics.js
+++ basics.js
@@ -1,11 +1,19 @@
-// get user to input num using prompte. check num is multiple of 5 or not
-
-
-let num = prompt('Enter a number for check the multiple:');
-if (num % 5 === 0) {
- console.log(num, 'is the multiple of 5');
+///////////Grades according to their scores
+let score = prompt ('enter a scores(0-100)');
+let grade;
+if (score >= 90 && score <= 100){
+ grade= "A";
 }
-else {
- console.log(num, 'is NOT the multiple of 5');
+else if (score >= 70 && score <= 89) {
+ grade= "B";
 }
-
+else if (score >= 60 && score <= 79) {
+ grade= "C";
+}
+else if (score >= 50 && score <= 59) {
+ grade= "D";
+}
+else if (score >= 0 && score<=49) {
+ grade= "F";
+}
+console.log('According to your scores: 90' + score+ ',your grade was:'+ grade);
