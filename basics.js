10053a64b058da52bc995a971daf8e97d58ea818
Sadaf <sadaf.marryum@gmail.com>
2024-10-09 18:02:45 +0500

    write code using conditional statement(check num is the  multiple of (5/any number) or not).

---
+++ basics.js
@@ -0,0 +1,11 @@
+// get user to input num using prompte. check num is multiple of 5 or not
+
+
+let num = prompt('Enter a number for check the multiple:');
+if (num % 5 === 0) {
+ console.log(num, 'is the multiple of 5');
+}
+else {
+ console.log(num, 'is NOT the multiple of 5');
+}
+
