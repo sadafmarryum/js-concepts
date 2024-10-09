b4633242c9d73afd0d2c8b54ac95df7d63de94c6
Sadaf <sadaf.marryum@gmail.com>
2024-10-09 18:09:08 +0500

    write code using for loop(calculate the sum of number).

--- basics.js
+++ basics.js
@@ -1,19 +1,9 @@
-///////////Grades according to their scores
-let score = prompt ('enter a scores(0-100)');
-let grade;
-if (score >= 90 && score <= 100){
- grade= "A";
+
+
+///////////Calculate the sum of 1 to n
+let n = prompt('Enter n for check the sum:');
+let sum = 0;
+for (let i = 1; i <= n; i++) {
+ sum = sum + i;
 }
-else if (score >= 70 && score <= 89) {
- grade= "B";
-}
-else if (score >= 60 && score <= 79) {
- grade= "C";
-}
-else if (score >= 50 && score <= 59) {
- grade= "D";
-}
-else if (score >= 0 && score<=49) {
- grade= "F";
-}
-console.log('According to your scores: 90' + score+ ',your grade was:'+ grade);
+console.log('sum of num is:' + sum);
